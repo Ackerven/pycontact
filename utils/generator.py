2818f436b00ba2b067ea00791ff4fd6197f7ceb9
Ackerven <ackerven@gmail.com>
2021-11-14 19:18:07 +0800

    complete generator

---
+++ utils/generator.py
@@ -0,0 +1,24 @@
+# @Author: Ackerven
+# @Mail: ackerven@cxmail.org
+# @Time: 14/11/2021 17:40
+# @File: generator.py
+# OS: Ubuntu 20.04 LTS
+# SoftWare: PyCharm
+# @Copyright Copyright(C) 2021 Ackerven All rights reserved.
+
+from faker import Faker
+from contact.contact import Contact
+import random
+
+fake = Faker(locale='zh_CN')
+
+
+def contact_generator(number):
+    contact_list = []
+    for i in range(number):
+        contact_list.append(Contact(fake.name(),
+                                    'Male' if random.randint(0, 1) > 0 else 'Female',
+                                    fake.phone_number(),
+                                    fake.first_romanized_name())
+                            )
+    return contact_list[:]
