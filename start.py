05e25c0d1a30b8d3013e87c21d78c663acb53482
Ackerven <ackerven@gmail.com>
2021-11-14 21:10:00 +0800

    start project

---
+++ start.py
@@ -0,0 +1,15 @@
+# @Author: Ackerven
+# @Mail: ackerven@cxmail.org
+# @Time: 14/11/2021 20:32
+# @File: start.py
+# OS: 
+# SoftWare: PyCharm
+# @Copyright Copyright(C) 2021 Ackerven All rights reserved.
+
+import yaml
+from user.console import console
+
+if __name__ == '__main__':
+    config = yaml.load(open('config.yaml', 'r', encoding='utf-8'), Loader=yaml.FullLoader)
+    if config['mode']['view'] == 'Console':
+        console()
