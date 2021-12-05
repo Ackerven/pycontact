a7e7906396d26713f4e6072919ac4f3848a3cf70
Ackerven <ackerven@gmail.com>
2021-12-05 11:05:24 +0800

    complete menu

--- view/gui.py
+++ view/gui.py
@@ -8,6 +8,32 @@
 
 import tkinter as tk
 
+
+# 菜单栏
+def Meun(root):
+    # 创建主菜单栏
+    menubar = tk.Menu(root)
+
+    # 创建菜单 tearoff 为 1 时可以独立出来
+    menufile = tk.Menu(menubar, tearoff=0)
+    menuset = tk.Menu(menubar, tearoff=0)
+    menuhelp = tk.Menu(menubar, tearoff=0)
+
+    # 把菜单作为层叠菜单添加到主菜单
+    menubar.add_cascade(label='文件', menu=menufile)
+    menubar.add_cascade(label='设置', menu=menuset)
+    menubar.add_cascade(label='帮助', menu=menuhelp)
+
+    # 添加菜单项到菜单
+    menufile.add_command(label='导入')
+    menufile.add_command(label='导出')
+    menuset.add_command(label='数据源')
+    menuhelp.add_command(label='关于')
+
+    # 把菜单栏添加到根窗口
+    root['menu'] = menubar
+
+
 def gui():
     # 创建主窗口并设置属性
     root = tk.Tk()
@@ -15,5 +41,8 @@ def gui():
     root['width'] = 542
     root['height'] = 442
     root.resizable(0, 0)
+
+    # 添加菜单栏
+    Meun(root)
 
     root.mainloop()
